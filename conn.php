c86cac0eb557fbe908e79adf431b5697fba1e3af
Wycliffe Nyachiro <georgewycs@gmail.com>
2018-06-25 13:07:03 +0300

    Changes

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', 'Character2', 'amsystem');
+	$conn = new mysqli('localhost', 'root', 'yourpassword', 'databasenae');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
